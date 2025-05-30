6bd7e673e8ea3801a49679986730e0865e6df59e
25LP9675 25LP9675 <25LP9675@Admins-iMac.local>
2025-05-30 09:52:08 +0800

    asd

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  BACKEND_ENDPOINT: process.env.BACKEND_ENDPOINT,
 };
 
 export default nextConfig;
